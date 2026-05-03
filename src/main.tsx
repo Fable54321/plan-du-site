84773040db864825fc4b78b4d5c9ad4cf825fd64
Timothe Bissonnette <timothe5@hotmail.com>
2026-05-03 05:47:22 -0400

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router/router'
-import App from '../src/100--App/App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
