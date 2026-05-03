57cc7097eca068d2f3e2a047f0fe98d03b0f1be1
Timothe Bissonnette <timothe5@hotmail.com>
2026-05-03 06:52:25 -0400

    first commit

--- src/api/visitorPlanApi.ts
+++ src/api/visitorPlanApi.ts
@@ -1,7 +1,7 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getVisitorPlan = async (token: string) => {
-  const response = await fetch(`${API_BASE_URL}/visitors/visitor-plan/${token}`);
+  const response = await fetch(`${API_BASE_URL}/plan/visitor-plan/${token}`);
 
   if (!response.ok) {
     throw new Error("Lien invalide ou expiré.");
